1aa03d84bfadc70683e4b8781196fe1f7480fc07
Brandddd <b.palacio1@utp.edu.co>
2022-12-20 17:37:51 -0500

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,27 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-        },
-    },
-});
+	// ----- server type ----
+	server: {
+		hmr: {
+			host: 'localhost'
+		}
+	},
+	plugins: [
+		laravel({
+			input: ['resources/sass/app.scss', 'resources/js/app.js'],
+			refresh: true
+		}),
+		vue()
+	],
+	resolve: {
+		alias: {
+			vue: 'vue/dist/vue.esm-bundler.js',
+			'@': path.resolve(__dirname, 'resources/js'),
+			'~': path.resolve(__dirname, 'node_modules')
+		}
+	}
+})
