cb399fb961412d9a34663f87e4bd11ad254cdb3e
Brandddd <b.palacio1@utp.edu.co>
2022-12-31 20:48:17 -0500

    feat(CRUD Categorías): Creación de crud de categorias
    Implementación por terminar del crud de catergorías, ya se crean por rutas api, y se pueden ver en la tabla todas las categorías por rutas web con su respectiva vista. Falta implementación de eliminación y actualización además de la vista modal de la creación

--- resources/js/components/Categories/CategoryIndex.vue
+++ resources/js/components/Categories/CategoryIndex.vue
@@ -1,13 +1,35 @@
 <template>
-	<h1>Hola componente Categorias tabla</h1>
+	<div class="card mx-5 my-5">
+		<div class="card-header d-flex justify-content-between">
+			<h2><strong>Tabla de categorías:</strong></h2>
+			<button class="btn btn-primary">Crear categoría</button>
+		</div>
+		<div class="card-body">
+			<!-- Load true -->
+			<section class="table-responsive" v-if="load">
+				<category-table :categories_data="categories" />
+			</section>
+			<!-- Load false -->
+			<section class="d-flex justify-content-center my-3" v-else>
+				<div class="spinner-border text-primary" role="status">
+					<span class="visually-hidden">Cargando...</span>
+				</div>
+			</section>
+		</div>
+	</div>
 </template>
 
 <script>
+import CategoryTable from './CategoryTableList.vue'
 export default {
 	props: ['category_data'],
+	components: {
+		CategoryTable
+	},
 	data() {
 		return {
-			categories: []
+			categories: [],
+			load: false
 		}
 	},
 	created() {
@@ -15,10 +37,11 @@ export default {
 	},
 	methods: {
 		index() {
-			this.getCategories
+			this.getCategories()
 		},
 		getCategories() {
 			this.categories = this.category_data
+			this.load = true
 		}
 	}
 }
---
+++ resources/js/components/Categories/CategoryTableList.vue
@@ -0,0 +1,92 @@
+<template>
+	<section class="intro">
+		<div class="mask d-flex align-items-center h-100">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-12">
+						<div class="card">
+							<div class="card-body p-0">
+								<div
+									class="table-responsive table-scroll"
+									data-mdb-perfect-scrollbar="true"
+									style="position: relative; height: 700px"
+								>
+									<table class="table table-striped mb-0 text-center">
+										<thead style="background-color: #002d72">
+											<tr>
+												<th scope="col">Identificador</th>
+												<th scope="col">Nombre categoría</th>
+												<th scope="col">Acciones</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr v-for="(category, index) in categories" :key="index">
+												<td>{{ index }}</td>
+												<td>{{ category.name }}</td>
+												<td>
+													<button class="btn btn-warning me-2">
+														Cambiar nombre
+													</button>
+													<button class="btn btn-danger">Eliminar</button>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	props: ['categories_data'],
+	data() {
+		return {
+			categories: []
+		}
+	},
+	created() {
+		this.index()
+	},
+	methods: {
+		index() {
+			this.categories = { ...this.categories_data }
+		},
+
+	}
+}
+</script>
+
+<style scoped>
+table td,
+table th {
+	text-overflow: ellipsis;
+	white-space: nowrap;
+	overflow: hidden;
+}
+
+thead th {
+	color: #fff;
+}
+
+.card {
+	border-radius: 0.5rem;
+}
+
+.table-scroll {
+	border-radius: 0.5rem;
+}
+
+.table-scroll table thead th {
+	font-size: 1.25rem;
+}
+thead {
+	top: 0;
+	position: sticky;
+}
+</style>
